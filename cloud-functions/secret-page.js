1932144c5662a261d81274bdd6cd84c9e367c7a4
batjargalaa <45119932+batjargalaa@users.noreply.github.com>
2021-10-05 18:01:20 +0800

    cloud function

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,22 @@
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to user's page"
-    })
+    const secretContent = `
+    <h3>Welcome to user's page</h3>
+    <p> only registered users</p>
+    `
+    let body
+    if (event.body) {
+        body = JSON.parse(event.body);
+    } else {
+        body = {}
+    }
+    if (body.password == "JavaScript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
 }
