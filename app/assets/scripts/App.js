f0679dc86751d9c582060248d8f148e660e9778d
batjargalaa <45119932+batjargalaa@users.noreply.github.com>
2021-10-05 08:03:08 +0800

    modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,28 @@
 import "../style/styles.css";
 import MobileMenu from "./modules/MoblileMenu";
+import Modal from "./modules/Modal";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let stickyHeader = new StickyHeader();
-// if (module.hot) {
-//     module.hot.accept();
-// }
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if (typeof modal == "undefined") {
+            import("./modules/Modal")
+                .then(x => {
+                    let modal = new x.default();
+                    setTimeout(() => modal.openModal(), 20)
+                })
+                .catch(() => console.log('aldaa garlaa'));
+        } else {
+            modal.openModal()
+        }
+
+    })
+});
+if (module.hot) {
+    module.hot.accept();
+}
